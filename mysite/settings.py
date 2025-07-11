375d8c1e235f1b3dd4cc681aa06a2089ece68d33
Deepadharshini1311 <deepakalai2004@gmail.com>
2025-07-11 10:32:48 +0530

    Force deploy for static fix

--- mysite/settings.py
+++ mysite/settings.py
@@ -117,7 +117,8 @@ USE_TZ = True
 import os
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS=[os.path.join(BASE_DIR, 'store','static')]
+STATICFILES_DIRS=[os.path.join(BASE_DIR,'static'),
+]
 STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
 
 
